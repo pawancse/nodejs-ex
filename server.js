791ebe9602924f4034d3673c2e5d532e10c6ddc9
pawan.kumar10 <pawan.bcet54@gmail.com>
2018-05-06 17:21:48 +0530

    DB URL update

--- server.js
+++ server.js
@@ -38,7 +38,7 @@ app.get('/viewProducts', function (req, res, next) {
 });
 var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
   ip = process.env.IP || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0',
-  mongoURL = process.env.OPENSHIFT_MONGODB_DB_URL || process.env.MONGO_URL || 'mongodb://127.0.0.1:27017',
+  mongoURL = process.env.OPENSHIFT_MONGODB_DB_URL || process.env.MONGO_URL,
   mongoURLLabel = "";
 if (mongoURL == null && process.env.DATABASE_SERVICE_NAME) {
   var mongoServiceName = process.env.DATABASE_SERVICE_NAME.toUpperCase(),
